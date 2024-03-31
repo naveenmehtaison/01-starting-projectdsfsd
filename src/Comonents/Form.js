143ebbe763d9518761cbc85fa7568bef2738e2ac
naveenmehtaison <naveenmehta2024@outlook.com>
2024-03-31 22:22:57 +0530

    CHossing_bus_voting

--- src/Comonents/Form.js
+++ src/Comonents/Form.js
@@ -1,11 +1,12 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef,useEffect } from "react";
 import Onscreen from "./Onscreen";
 
 const Form = () => {
     const [entereddata, setentereddata] = useState([]);
-    const [submitted, setsubmitted] = useState(false);
 
-
+    const Nameref = useRef()
+    const Emailref = useRef()
+    const Phoneref = useRef()
 
     const handleFormSubmit = (event) => {
         console.log('iam inside hanlde frim submit')
@@ -16,52 +17,67 @@ const Form = () => {
             Bus: event.target.elements.selector.value,
             Email:event.target.elements.email.value,
             Phone:event.target.elements.number.value,
-            key:3
+            key:3,
         };
 
         setentereddata([form_data,...entereddata]);
-        <Onscreen totaldata={entereddata} />
-
+    };
+    const deleteHandler = (item) => {
+        setentereddata(entereddata.filter((_, i) => i !== item));
 
     };
-    const editHandler = (ele) => {
-        // You can update the entereddata array directly
-        setentereddata(entereddata.map(item => {
-            if (item === ele) {
-                return {
-                    ...item,
-                    Name: ele.Name,
-                    Bus: ele.Bus,
-                    Email: ele.Email,
-                    Phone: ele.Phone
-                };
-            }
-            return item;
-        }));
+    const editHandler = (item,ele) => {
+        setentereddata(entereddata.filter((_,i)=>i!==item))
+        Nameref.current.value=ele.Name
+        Emailref.current.value=ele.Email
+        Phoneref.current.value=ele.Phone
+
     };
-    console.log(entereddata)
+    const [filteryear,setfilteryear]=useState('all')
+    const FilterYearFunc2=(event)=>{
+        setfilteryear(event.target.value)
+        console.log('im inside filtertearfunc2')
+    }
+
+    const FilterYearFunc= (entereddata=>{
+        if(filteryear==="all"){
+            return entereddata
+        }
+        else{
+            return(entereddata.filter(item=>item.Bus===filteryear))
+        }      
+    })
+
 
     return (
         <div>
+            <h1>BUS Booking</h1>
+            <select onChange={(event)=>FilterYearFunc2(event)} id='selector0'>
+                <option id='all' value='all'>All</option>
+                <option id='Bus10' value='Bus1'>BUS1</option>
+                <option id="BUs20" value='Bus2'>BUS2</option>
+                <option id="Bus30" value='Bus3'>Bus3</option>
+            </select>
+            <br></br>
             <form onSubmit={handleFormSubmit}>
-                <h1>BUS Booking</h1>
+
                 
                 <label htmlFor="name">Name</label>
-                <input type='text' id='name'></input>
+                <input ref={Nameref}type='text' id='name'></input>
                 <select id='selector'>
                     <option id='Bus1' value='Bus1'>BUS1</option>
                     <option id="BUs2" value='Bus2'>BUS2</option>
-                    <option id="Bus3" value='abhik'>Bus3</option>
+                    <option id="Bus3" value='Bus3'>Bus3</option>
                 </select>
                 <label htmlFor="number">PHONE</label>
-                <input type='number' id='number'></input>
+                <input  ref={Phoneref}type='number' id='number'></input>
                 <label htmlFor="email">Email</label>
-                <input id='email' type='email'></input>
+                <input ref = {Emailref} id='email' type='email'></input>
                 <button type='submit'>submit</button>
 
 
             </form>
-             <Onscreen totaldata={entereddata} />
+             <Onscreen totaldata={FilterYearFunc(entereddata)} Ondelete={deleteHandler} onEdit={editHandler} />
 
         </div>
     );
--- src/Comonents/Onscreen.js
+++ src/Comonents/Onscreen.js
@@ -1,50 +1,27 @@
-// import React, { useState } from "react"
-// const Onscreen=(props)=>{
-    
-//     const [totaldata,setTotalData]=useState(props.totaldata)
 
-//     const deletehandler = (item) => {
-//         setTotalData(totaldata.filter((_, i) => i !== item));
-//     };
-//     console.log(totaldata)
-    
-//     return(
-//         <div>
-//             {totaldata.map((ele,item)=>(
-//                 <div key={item}>
-//                     <p>{ele.Name}</p>
-//                     <p>{ele.Bus}</p>
-//                     <p>{ele.Email}</p>
-//                     <p>{ele.Phone}</p>
-//                     <button onClick={()=>deletehandler(item)} >Delete</button>
-//                     <button>Edit</button>
-//                 </div>
-//             ))}
-//         </div>
-
-
-//     )
-
-// }
-// export default Onscreen
 import React, { useState } from "react";
 
 const Onscreen = (props) => {
+    console.log(props.Ondelete)
+    
 
-    const [totaldata, setTotalData] = useState(props.totaldata);
+    // const [totaldata1, setTotalData] = useState(props.totaldata);
 
     const deleteHandler = (item) => {
-        setTotalData(totaldata.filter((_, i) => i !== item));
+        // console.log(props.Ondelete)
+        // setTotalData(totaldata1.filter((_, i) => i !== item));
+        props.Ondelete(item)
     };
     const Edithandler =(item,ele)=>{
-        props.onEdit(ele)
-        setTotalData(totaldata.filter((_,i)=>i!==item))
+        props.onEdit(item,ele)
+        // setTotalData(totaldata1.filter((_,i)=>i!==item))
         
     }
-    
+
     return (
         <div>
-            {totaldata.map((ele, item) => (
+            {props.totaldata.map((ele, item) => (
+
                 <div key={item}>
                     <p>{ele.Name}</p>
                     <p>{ele.Bus}</p>
